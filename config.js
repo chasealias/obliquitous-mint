d6b5bdec5dc0f464939e885f3f2c0401c3ef9900
Chase Alias <pollack@altart.io>
2025-06-07 02:25:28 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export const MINT_CONTRACT = "0xC489d5eA1174Bbe4330fd7082779969096403cF2";
+export const TOKEN_CONTRACT = "0x08e71f813cb60953a15843d4f397c61649a62277";
+
+export const NETWORK = {
+  chainId: '0x2105', // Base Mainnet
+  rpcUrls: ['https://mainnet.base.org'],
+};
