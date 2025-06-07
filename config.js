6921c0538b02b7a5bfcbcb0d4e8a2801816a336d
Chase Alias <chasealias@outlook.com>
2025-06-07 10:27:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,69 @@
+// === Contract Addresses ===
 export const MINT_CONTRACT = "0xC489d5eA1174Bbe4330fd7082779969096403cF2";
 export const TOKEN_CONTRACT = "0x08e71f813cb60953a15843d4f397c61649a62277";
 
+// === Network Info (Base Mainnet) ===
 export const NETWORK = {
-  chainId: '0x2105', // Base Mainnet
-  rpcUrls: ['https://mainnet.base.org'],
+  chainId: "0x2105", // Hex for 8453
+  chainName: "Base Mainnet",
+  nativeCurrency: {
+    name: "ETH",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: ["https://mainnet.base.org"],
+  blockExplorerUrls: ["https://basescan.org"],
 };
+
+// === Mint Contract ABI (Minimal) ===
+export const MINT_ABI = [
+  {
+    "inputs": [],
+    "name": "mint",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+// === ERC-20 Token ABI (Minimal) ===
+export const TOKEN_ABI = [
+  {
+    "constant": true,
+    "inputs": [{ "name": "account", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "type": "function",
+    "stateMutability": "view"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [{ "name": "", "type": "string" }],
+    "type": "function",
+    "stateMutability": "view"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [{ "name": "", "type": "uint8" }],
+    "type": "function",
+    "stateMutability": "view"
+  }
+];
